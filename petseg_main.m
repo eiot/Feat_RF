73035fcc24f905272df3bb803b2a6d1bac8d6237
eiot.github.io <775401754@qq.com>
2016-07-20 13:36:04 +0800

    3th
    Compiled mexClassRF_predict.mexa64

--- petseg_main.m
+++ petseg_main.m
@@ -1,34 +1,44 @@
-function SegResult = petseg_run_new(input_dir,load_dir,output_dir)
+function SegResult = petseg_main(input_dir,load_dir,output_dir)
 % The main function of PetSeg
 % INPUT:   input_dir   load the PET images to be segmented
 %          load_dir    load the mat files
 % OUTPUT:  segedmat	 
 
-% examples:
+% examples(Window):
 % input_dir='D:\data\PET_liu\send\data\test_input\';
 % load_dir='D:\data\PET_liu\send\data\trained_model\';
 % output_dir='D:\data\PET_liu\send\data\output\';
 
+% examples(Linux):
+% input_dir='/mysoft/PET_liu/Feat_RF-master/data/test_input/';
+% load_dir='/mysoft/PET_liu/Feat_RF-master/data/trained_model/';
+% output_dir='/mysoft/PET_liu/Feat_RF-master/data/output/';
 
+if strcmpi(computer,'PCWIN') |strcmpi(computer,'PCWIN64')
+   file_slash='\';
+else
+   file_slash='/';
+end
 dirname = fileparts(mfilename('fullpath')); % Current directory
 addpath(genpath(dirname));  % Add all subdirectory
 cd(dirname);
 
 cd(input_dir); 
-all_list=[dir('C*');dir('P*');dir('S*')];
+all_list=[dir('C*');dir('P*');dir('S*');dir('c*');dir('p*');dir('s*')];
 
-for i=1:3
+for i=1:size(all_list,1)
+
     file_type=all_list(i).name;
-    cd([input_dir file_type '\']);
+    cd([input_dir file_type file_slash]);
     type_list=dir(['*',file_type(2:end),'*']);
     for j=1:size(type_list,1)
         pat_folder=type_list(j).name;
-        cd([input_dir file_type '\' pat_folder '\']);
+        cd([input_dir file_type file_slash pat_folder file_slash]);
         nii_list=dir('*.nii');
         for k=1:size(nii_list,1)
-            original_file=[input_dir file_type '\' pat_folder '\' nii_list(k).name];
+            original_file=[input_dir file_type file_slash pat_folder file_slash nii_list(k).name];
             TestImg=load_nii(original_file);
-            disp('Get the features of the test sample:');
+            %disp('Get the features of the test sample:');
             feat_gray=featextract(TestImg.img,'gray');
             switch(file_type)
                 case 'Clinical'
@@ -41,20 +51,16 @@ for i=1:3
                     load([load_dir 'Model_Gray_Simu.mat']);
                     X_tst=feat_gray; 
             end
-            disp('Classificating the voxels:');
+            %disp('Classificating the voxels:');
             Y_hat=classRF_predict(X_tst,model);
             temp=TestImg;
             SegResult=GetSegResult(temp.img,Y_hat);
             temp.img=SegResult;
             
-            seged_dir=[output_dir '\' file_type '\' pat_folder];
+            seged_dir=[output_dir file_slash file_type file_slash pat_folder];
             if ~(exist(seged_dir, 'dir') )        mkdir(seged_dir);  end
             
-            save_nii(temp,[seged_dir '\Result_' nii_list(k).name(1:end-4) '.nii']);
+            save_nii(temp,[seged_dir file_slash 'Result_' nii_list(k).name(1:end-4) '.nii']);
         end        
     end
 end
-
-
-
-
